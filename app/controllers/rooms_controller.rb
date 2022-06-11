d9e263e078967ab05406348b0e7ef012021c9b88
emachipana <emachipana0102@gmail.com>
2022-06-11 17:55:45 -0500

    FIX: render in format json

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -25,11 +25,9 @@ class RoomsController < ApplicationController
 
     respond_to do |format|
       if @room.save
-        format.html { redirect_to room_url(@room), notice: "Room was successfully created." }
-        format.json { render :show, status: :created, location: @room }
+        render json: @room, status: :created
       else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @room.errors, status: :unprocessable_entity }
+        render json: @room.errors, status: :unprocessable_entity 
       end
     end
   end
@@ -38,11 +36,9 @@ class RoomsController < ApplicationController
   def update
     respond_to do |format|
       if @room.update(room_params)
-        format.html { redirect_to room_url(@room), notice: "Room was successfully updated." }
-        format.json { render :show, status: :ok, location: @room }
+        render json: :show, status: :ok
       else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @room.errors, status: :unprocessable_entity }
+        render json: @room.errors, status: :unprocessable_entity
       end
     end
   end
@@ -50,11 +46,6 @@ class RoomsController < ApplicationController
   # DELETE /rooms/1 or /rooms/1.json
   def destroy
     @room.destroy
-
-    respond_to do |format|
-      format.html { redirect_to rooms_url, notice: "Room was successfully destroyed." }
-      format.json { head :no_content }
-    end
   end
 
   private
