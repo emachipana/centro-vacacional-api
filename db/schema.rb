f3414962d9ad10a2b9378a37ea7144727e7dfae9
emachipana <emachipana0102@gmail.com>
2022-06-11 18:54:49 -0500

    ADD: scaffolding clients

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_11_230439) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_11_235127) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "clients", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.integer "phone_number"
+    t.integer "DNI"
+    t.integer "expenses", default: 0
+    t.bigint "room_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["room_id"], name: "index_clients_on_room_id"
+  end
 
   create_table "foods", force: :cascade do |t|
     t.string "name"
@@ -37,4 +50,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_11_230439) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "clients", "rooms"
 end
