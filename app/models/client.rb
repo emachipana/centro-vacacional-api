2b648f2a1635aae0ebbfb5edf571c3c25f6cf963
emachipana <emachipana0102@gmail.com>
2022-06-11 23:45:28 -0500

    ADD: validates to client

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,4 +1,12 @@
 class Client < ApplicationRecord
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email,
+    format: { with: URI::MailTo::EMAIL_REGEXP },
+    allow_nil: true,
+    uniqueness: true
+  validates :DNI presence: true, uniqueness: true
+
   belongs_to :room
   has_many :orders, dependent: :destroy
   has_one :user
