813ecb8a380d4c3367e489bebbc91af442b40fec
emachipana <emachipana0102@gmail.com>
2022-06-11 19:17:37 -0500

    FIX: only json format

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -23,38 +23,25 @@ class OrdersController < ApplicationController
   def create
     @order = Order.new(order_params)
 
-    respond_to do |format|
       if @order.save
-        format.html { redirect_to order_url(@order), notice: "Order was successfully created." }
-        format.json { render :show, status: :created, location: @order }
+        render :show, status: :created
       else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @order.errors, status: :unprocessable_entity }
+        render json: @order.errors, status: :unprocessable_entity
       end
-    end
   end
 
   # PATCH/PUT /orders/1 or /orders/1.json
   def update
-    respond_to do |format|
       if @order.update(order_params)
-        format.html { redirect_to order_url(@order), notice: "Order was successfully updated." }
-        format.json { render :show, status: :ok, location: @order }
+        render :show, status: :ok, location: @order
       else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @order.errors, status: :unprocessable_entity }
+        render json: @order.errors, status: :unprocessable_entity
       end
-    end
   end
 
   # DELETE /orders/1 or /orders/1.json
   def destroy
     @order.destroy
-
-    respond_to do |format|
-      format.html { redirect_to orders_url, notice: "Order was successfully destroyed." }
-      format.json { head :no_content }
-    end
   end
 
   private
