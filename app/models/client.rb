05dcadb3a1864e9b316d832fb52b75ab91b52cc3
emachipana <emachipana0102@gmail.com>
2022-06-12 00:21:51 -0500

    FIX: typo after validates DNI

--- app/models/client.rb
+++ app/models/client.rb
@@ -5,7 +5,7 @@ class Client < ApplicationRecord
     format: { with: URI::MailTo::EMAIL_REGEXP },
     allow_nil: true,
     uniqueness: true
-  validates :DNI presence: true, uniqueness: true
+  validates :DNI, presence: true, uniqueness: true
 
   belongs_to :room
   has_many :orders, dependent: :destroy
