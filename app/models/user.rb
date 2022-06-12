cf871b7a53f949374b6095d0074de42756349f65
emachipana <emachipana0102@gmail.com>
2022-06-11 20:37:06 -0500

    ADD: sessions controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
 class User < ApplicationRecord
+  has_secure_password
+  has_secure_token
   belongs_to :client
+
+  def invalidate_token
+    update(token: nil)
+  end
+
+  def self.valid_login?(username, password)
+    user = find_by(username: username)
+    user&.authenticate(password)
+  end
 end
