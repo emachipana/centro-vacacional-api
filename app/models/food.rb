79817e78f87068b464f08290b8dfa5a51f534cc5
emachipana <emachipana0102@gmail.com>
2022-06-12 00:37:21 -0500

    ADD: validates for foods

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,4 +1,8 @@
 class Food < ApplicationRecord
+  validates :name, presence: true
+  validates :description, presence: true, length: { maximum: 500 }
+  enum type_food: { breakfast: 0, lunch: 1, dinner: 2 }
+
   has_one_attached :banner
   has_many :orders, dependent: :destroy
 end
