095b8e52a457965fc389776ee3a248f2bc35a94e
emachipana <emachipana0102@gmail.com>
2022-06-11 23:34:09 -0500

    ADD: index to email in clients

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_12_032317) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_12_043322) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -52,6 +52,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_12_032317) do
     t.bigint "room_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_clients_on_email", unique: true
     t.index ["room_id"], name: "index_clients_on_room_id"
   end
 
