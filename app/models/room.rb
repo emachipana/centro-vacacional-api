c550f8edfa7cddb80196ea8f3cb521219366ac62
emachipana <emachipana0102@gmail.com>
2022-06-12 00:20:32 -0500

    ADD: validates of room

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,7 @@
 class Room < ApplicationRecord
+  validates :num_room, presence: true, uniqueness: true
+  validates :description, presence: true, length: { maximum: 500 }
+
   has_one_attached :banner
   has_one :client
 end
