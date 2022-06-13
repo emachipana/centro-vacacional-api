1d37875f95b973e624b5dc7ab7bf7ca71cd861cd
emachipana <emachipana0102@gmail.com>
2022-06-12 19:48:37 -0500

    UPDATE: add column type of user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_13_000523) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_13_004727) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -102,6 +102,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_13_000523) do
     t.bigint "client_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "type_user", default: 0
     t.index ["client_id"], name: "index_users_on_client_id"
     t.index ["token"], name: "index_users_on_token"
     t.index ["username"], name: "index_users_on_username", unique: true
