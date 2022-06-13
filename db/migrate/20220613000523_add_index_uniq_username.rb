c361c06270749463786769d0cb65a153d243722b
emachipana <emachipana0102@gmail.com>
2022-06-12 19:07:03 -0500

    ADD: Index uniq to username in users

---
+++ db/migrate/20220613000523_add_index_uniq_username.rb
@@ -0,0 +1,5 @@
+class AddIndexUniqUsername < ActiveRecord::Migration[7.0]
+  def change
+    add_index :users, :username, unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_12_054225) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_13_000523) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -104,6 +104,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_12_054225) do
     t.datetime "updated_at", null: false
     t.index ["client_id"], name: "index_users_on_client_id"
     t.index ["token"], name: "index_users_on_token"
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
