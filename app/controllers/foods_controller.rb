ee7368f59078dcea808495c649df3b277f6af0c0
emachipana <emachipana0102@gmail.com>
2022-06-12 20:08:25 -0500

    FIX: controller foods

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,5 +1,6 @@
 class FoodsController < ApplicationController
-  before_action :set_food, only: %i[ show edit update destroy ]
+  before_action :authorize_user
+  before_action :set_food, only: %i[ show update destroy ]
 
   # GET /foods or /foods.json
   def index
@@ -10,21 +11,12 @@ class FoodsController < ApplicationController
   def show
   end
 
-  # GET /foods/new
-  def new
-    @food = Food.new
-  end
-
-  # GET /foods/1/edit
-  def edit
-  end
-
   # POST /foods or /foods.json
   def create
     @food = Food.new(food_params)
 
     if @food.save
-      render json: :show, status: :created
+      render json: @food, status: :created
     else
       render json: @food.errors, status: :unprocessable_entity
     end
@@ -33,7 +25,7 @@ class FoodsController < ApplicationController
   # PATCH/PUT /foods/1 or /foods/1.json
   def update
     if @food.update(food_params)
-      render :show, status: :ok
+      render @food, status: :ok
     else
       render json: @food.errors, status: :unprocessable_entity
     end
@@ -53,5 +45,12 @@ class FoodsController < ApplicationController
     # Only allow a list of trusted parameters through.
     def food_params
       params.require(:food).permit(:name, :price, :amount, :description, :primer, :type, :state)
+    end
+
+    def authorize_user
+      return if currrent_user
+
+      errors = { errors: { message: "Acces denied" } }
+      render json: errors, status: :unauthorized
     end
 end
